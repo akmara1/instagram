63042af2cfd63a3dd388db641baddc4adcc4238e
akmara1 <95084923+akmara1@users.noreply.github.com>
2022-05-15 09:25:23 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
+image = input("Input name of image file")
+
+im = Image.open(image)
+im = im.resize((1080, 1080))
+
+draw = ImageDraw.Draw(im)
+draw.text((0,0), "watermark", (255, 255, 255))
+plt.subplot(1, 2, 1)
+plt.imshow(im)
